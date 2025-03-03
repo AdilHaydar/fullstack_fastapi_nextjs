30e3ccf577b475c52850b3827b609e538d906e35
unknown <adilhaydarr@gmail.com>
2025-03-03 19:28:33 +0300

    first commşt

---
+++ README.md
@@ -0,0 +1,1 @@
+"# fullstack_fastapi_nextjs" 
---
+++ backend/api/deps.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+import os
+from .database import SessionLocal
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
+ALGORITHM = os.getenv("AUTH_ALGORITHM")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
